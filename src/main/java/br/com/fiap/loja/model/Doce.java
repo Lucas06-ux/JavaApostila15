44e14648064e5d25cab2175c8d45bf00544975e3
Lucas06-ux <lubgian@gmail.com>
2025-10-06 15:10:41 -0300

    Usando quarkus para conextar a API no java

---
+++ src/main/java/br/com/fiap/loja/model/Doce.java
@@ -0,0 +1,51 @@
+package br.com.fiap.loja.model;
+
+import java.time.LocalDate;
+
+public class Doce {
+    private int codigo;
+    private String nome;
+    private double valor;
+    private double peso;
+    private LocalDate dataValidade;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public LocalDate getDataValidade() {
+        return dataValidade;
+    }
+
+    public void setDataValidade(LocalDate dataValidade) {
+        this.dataValidade = dataValidade;
+    }
+}
